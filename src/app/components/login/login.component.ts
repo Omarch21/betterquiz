362df345c64846561d45325aef17636e361d340a
omarch21 <omarch641@gmail.com>
2023-04-23 01:31:33 -0500

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,4 +12,21 @@ export class LoginComponent {
     password: new FormControl(null,Validators.required)
   }
     )
+    changeTab(tabName: string) {
+      const loginbody = document.querySelector('.bodylogin');
+      const signupbody = document.querySelector('.bodysignup');
+      const loginTab = document.querySelector('.loginTab');
+      const signupTab = document.querySelector('.signupTab');
+      if (tabName === 'signup') {
+          loginTab?.classList.remove('active');
+          signupTab?.classList.add('active');
+          loginbody?.classList.remove('active');
+          signupbody?.classList.add('active');
+      } else if (tabName === 'login') {
+          loginTab?.classList.add('active');
+          signupTab?.classList.remove('active');
+          loginbody?.classList.add('active');
+          signupbody?.classList.remove('active');
+      }
+  }
 }
