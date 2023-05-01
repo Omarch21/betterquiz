52d8ad91cc9be22a71febca0cd1a1b421386e397
omarch21 <omarch641@gmail.com>
2023-04-30 22:18:00 -0500

    fix

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild,ElementRef,AfterViewInit,OnInit, Renderer2 } from '@angular/core';
 import { UsersService } from 'src/app/services/users.service';
 
 @Component({
@@ -6,15 +6,55 @@ import { UsersService } from 'src/app/services/users.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
-constructor(private usersService: UsersService){}
+export class DashboardComponent implements OnInit{
+  
+
+constructor(private usersService: UsersService, private renderer: Renderer2){}
 user$ = this.usersService.getCurrentAccountProfile$;
 card: HTMLElement |  null = null;
-flipCard(event: EventTarget| null){
-  if(event){
-  const cardContainer = event as HTMLElement;
-  cardContainer.classList.toggle('flipped');
+cardindex = 0;
+cardwidth:number = 0;
+cards: HTMLElement[] = [];
+flipCard(event: Event){
+  const cardContainer = (event.target as HTMLElement).closest('.card-container');
+  if(cardContainer){
+    cardContainer.classList.toggle('flipped');
+  }
+}
+ngOnInit():void{
+  const cardContainer = document.querySelectorAll('.card-container') as NodeListOf<HTMLElement>
+  if(cardContainer.length > 0){
+    this.cardwidth = cardContainer[0].offsetWidth ;
+    for (let i = 0; i < cardContainer.length; i++) {
+      this.renderer.setStyle(cardContainer[i], 'width', `${this.cardwidth}px`);
+      this.cards.push(cardContainer[i]);
+    }
 
   }
 }
+
+
+
+nextCard(): void {
+  if (this.cardindex === this.cards.length - 1) {
+    this.cardindex = 0;
+  } else {
+    this.cardindex++;
+  }
+  const leftPosition = -(this.cardindex * this.cardwidth) ;
+  const carousel = document.querySelector('.card-carousel') as HTMLElement;
+  carousel.style.transform = `translateX(${leftPosition}px)`;
+  console.log(this.cardwidth);
 }
+
+prevCard(): void {
+  if (this.cardindex === 0) {
+    this.cardindex = this.cards.length - 1;
+  } else {
+    this.cardindex--;
+  }
+  const leftPosition = -(this.cardindex * this.cardwidth);
+  const carousel = document.querySelector('.card-carousel') as HTMLElement;
+  carousel.style.transform = `translateX(${leftPosition}px)`;
+}
+  }
