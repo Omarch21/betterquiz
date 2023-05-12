14f7e6c8909433a1297838f873cd8f115746b338
omarch21 <omarch641@gmail.com>
2023-05-11 22:17:26 -0500

    fix

---
+++ src/app/components/notecards-set-menu/notecards-set-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notecards-set-menu',
+  templateUrl: './notecards-set-menu.component.html',
+  styleUrls: ['./notecards-set-menu.component.css']
+})
+export class NotecardsSetMenuComponent {
+
+}
